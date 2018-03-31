a6e40742863da1e334a4dcbff67351fd737c0945
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2018-03-31 02:22:55 -0700

    Finished up to step 3

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,8 +11,19 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-
+ if (cardNumber.length === 14 && (cardNumber.slice(0,2) === '38' || cardNumber.slice(0,2) === '39')) {
+ 	return 'Diner\'s Club';
+ } else if (cardNumber.length === 15 && (cardNumber.slice(0,2) === '34' || cardNumber.slice(0,2) === '37')) {
+ 	return 'American Express';
+ } else if (cardNumber[0] === '4' && (cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19)) {
+ 	return 'Visa';
+ } else if (cardNumber.length === 16 && (cardNumber.slice(0,2) === '51' || cardNumber.slice(0,2) === '52' || cardNumber.slice(0,2) === '53' || cardNumber.slice(0,2) === '54' || cardNumber[0] + cardNumber[1] === '55')) {
+ 	return 'MasterCard';
+ } else if ((cardNumber.length === 16 || cardNumber.length === 19) && (cardNumber.slice(0,4) === '6011' || cardNumber.slice(0, 3) === '644' || cardNumber.slice(0, 3) === '645' || cardNumber.slice(0, 3) === '646' || cardNumber.slice(0, 3) === '647' || cardNumber.slice(0, 3) === '648' || cardNumber.slice(0, 3) === '649' || cardNumber.slice(0,2) === '65')) {
+ 	return 'Discover';
+ } else if ((cardNumber.length >= 12 && cardNumber.length <= 19) && (cardNumber.slice(0,4) === '5018' || cardNumber.slice(0,4) === '5020' || cardNumber.slice(0,4) === '5038' || cardNumber.slice(0,4) === '6304')) {
+ 	return 'Maestro';
+ }
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
-
 
